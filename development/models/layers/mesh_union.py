1d4c51917e3153fe111aa7243e5444d2b6c686ca
Andres Mandado <andres.mandado-almajano@here.com>
2020-12-12 22:28:23 +0100

    Sparse implementation of MeshUnion and MeshUnpool (WIP).

--- development/models/layers/mesh_union.py
+++ development/models/layers/mesh_union.py
@@ -19,85 +19,91 @@ class MeshUnion:
         self.groups[target, :] += self.groups[source, :]
         #self.sparseGroups[target].add_(self.sparseGroups[source]) doesn't work
 
-        targetRow = self.sparseGroups[target].clone()
-        srcRow = self.sparseGroups[source].clone()
-
         self.sparseGroups = self.sparseGroups.coalesce()
-        sparseRow = self.sparseGroups[source]
-        rowIdcs = torch.clone(sparseRow._indices())
-        rowValues = torch.clone(sparseRow._values())
+        sparseRow = self.sparseGroups[source].coalesce()
+        rowIdcs = torch.clone(sparseRow.indices())
+        rowValues = torch.clone(sparseRow.values())
         allIdcs = torch.cat(((torch.ones(rowIdcs.shape[1], dtype=torch.long, device = self.device) * target).unsqueeze(0), rowIdcs))
-        self.sparseGroups.add_(torch.sparse_coo_tensor(allIdcs,rowValues,self.sparseGroups.shape))
-
-        assert torch.all(torch.eq(self.sparseGroups[target].to_dense(),targetRow.add(srcRow).to_dense())), "Union wrong"
+        tmpSparseTensor = torch.sparse_coo_tensor(allIdcs,rowValues,self.sparseGroups.shape)
+        self.sparseGroups.add_(tmpSparseTensor)
+        self.sparseGroups = self.sparseGroups.coalesce()
 
     def remove_group(self, index):
-        # self.sparseGroups = self.sparseGroups.coalesce()
-        # sparseRow = self.sparseGroups[index]
-        # rowIdcs = torch.clone(sparseRow._indices())
-        # rowValues = torch.clone(sparseRow._values())
-        # allIdcs = torch.cat(((torch.ones(rowIdcs.shape[1], dtype=torch.long, device = self.device) * index).unsqueeze(0), rowIdcs))
-        # self.sparseGroups.sub_(torch.sparse_coo_tensor(allIdcs, rowValues, self.sparseGroups.shape))
-        #
-        # assert torch.count_nonzero(self.sparseGroups[index].to_dense()) == 0, "Not removed"
         return
 
     def get_group(self, edge_key):
-        return self.groups[edge_key, :]
+        # This is actually never called.
+        return self.sparseGroups[edge_key].to_dense()
 
     def get_occurrences(self):
-        return torch.sum(self.groups, 0)
+        sum = torch.sparse.sum(self.sparseGroups, 0).to_dense()
+        assert torch.allclose(sum, torch.sum(self.groups, 0)), "Wrong sum"
+        return sum
 
     def get_groups(self, tensor_mask):
         self.groups = torch.clamp(self.groups, 0, 1)
+        self.sparseGroups = self.sparseGroups.coalesce()
+        self.sparseGroups.values().clamp_(0,1)
         return self.groups[tensor_mask, :]
+
+    def get_sparse_groups(self):
+        self.sparseGroups = self.sparseGroups.coalesce()
+        self.sparseGroups.values().clamp_(0, 1)
+        return self.sparseGroups
 
     def rebuild_features_average(self, features, mask, target_edges):
         self.prepare_groups(features, mask)
         fe = torch.matmul(features.squeeze(-1), self.groups)
+
+        tensor_mask = torch.from_numpy(mask)
+        self.sparseGroups = self.sparseGroups.coalesce()  # really needed?
+
+        sparseFe = torch.sparse.mm(self.sparseGroups.transpose(1, 0), features.squeeze(-1).transpose(1, 0))[tensor_mask,:].transpose_(1, 0)
+
+        #assert torch.allclose(sparseFe, fe), "Tensors are not equal"
+
         occurrences = torch.sum(self.groups, 0).expand(fe.shape)
         fe = fe / occurrences
 
-        tensor_mask = torch.from_numpy(mask)
-        self.sparseGroups = self.sparseGroups.coalesce() # really needed?
-        sparseFe = torch.sparse.mm(self.sparseGroups.transpose(1,0), features.squeeze(-1).transpose(1,0))[tensor_mask, :].transpose_(1,0)
-        sparseOccurrences = torch.sparse.sum(self.sparseGroups,0).to_dense()[tensor_mask];
-        sparseOccurrences = sparseOccurrences.expand(fe.shape[0],sparseOccurrences.shape[0]) # could we broadcast instead of expand??
+        sparseOccurrences = torch.sparse.sum(self.sparseGroups,0).to_dense()[tensor_mask].expand(sparseFe.shape) # could we broadcast instead of expand??
+
+        #assert torch.allclose(occurrences,sparseOccurrences), "Occurrences not equal"
+
         sparseFe = sparseFe / sparseOccurrences
-        #sparseFe = sparseFe[:, torch.from_numpy(mask)]
 
         padding_b = target_edges - fe.shape[1]
         if padding_b > 0:
             padding_b = ConstantPad2d((0, padding_b, 0, 0), 0)
             fe = padding_b(fe)
             sparseFe = padding_b(sparseFe)
-        if (not torch.allclose(sparseFe,fe)):
-            diffIndices = torch.nonzero(torch.sub(sparseFe,fe) > 0.01)
-            print(len(diffIndices), "diffs of", fe.shape[0]*fe.shape[1])
-            assert False, "Tensors are not equal"
-        return fe
+        assert torch.allclose(sparseFe, fe), "Tensors are not equal"
+        # if (not torch.allclose(sparseFe,fe)):
+        #     diffIndices = torch.nonzero(torch.sub(sparseFe,fe) > 0.01)
+        #     print(len(diffIndices), "diffs of", fe.shape[0]*fe.shape[1])
+        #     assert False, "Tensors are not equal"
+        return sparseFe
 
     def prepare_groups(self, features, mask):
-        #n = len(self.groupIndices)
         padded_n = features.shape[1]
-        # indices = torch.cat((torch.arange(0,n,device=self.device).unsqueeze(0),self.groupIndices.unsqueeze(0)))
-        # values = torch.ones(n,device=self.device)
-        # self.sparseGroups = torch.sparse_coo_tensor(indices,values,(n,padded_n),device=self.device)
-
-        assert torch.max(self.groups) == 1.0, "Values greater than 1"
-        assert torch.min(self.groups) == 0.0, "Values less than 0"
 
         tensor_mask = torch.from_numpy(mask)
 
-        assert torch.allclose(self.groups, self.sparseGroups.to_dense()), "Error"
+        self.sparseGroups = self.sparseGroups.coalesce()
+        self.sparseGroups.values().clamp_(0, 1)
+        # sp = self.sparseGroups.to_dense()
+        #
+        # if (not torch.allclose(self.groups,sp )):
+        #     diffIndices = torch.nonzero(torch.abs(torch.sub(self.groups,sp)) > 0.00001)
+        #     print(len(diffIndices), "diffs of", sp.shape[0]*sp.shape[1])
+        #     assert False, "Tensors are not equal"
+        #assert torch.allclose(self.groups[tensor_mask,:],self.sparseGroups.to_dense()[tensor_mask,:]), "Error"
 
-        assert torch.allclose(self.groups[tensor_mask,:],self.sparseGroups.to_dense()[tensor_mask,:]), "Error"
         self.groups = torch.clamp(self.groups[tensor_mask, :], 0, 1).transpose_(1, 0)
-
-
+        self.sparseGroups = self.sparseGroups.transpose_(1, 0) # Do masking later, on multiplication.
 
         padding_a = features.shape[1] - self.groups.shape[0]
         if padding_a > 0:
             padding_a = ConstantPad2d((0, 0, 0, padding_a), 0)
             self.groups = padding_a(self.groups)
             self.sparseGroups.sparse_resize_((padded_n,self.sparseGroups.shape[1]),2,0)
+            #assert torch.allclose(self.groups, self.sparseGroups.to_dense()[:,tensor_mask]), "Error"
--- development/models/layers/mesh_unpool.py
+++ development/models/layers/mesh_unpool.py
@@ -1,2 +1,78 @@
-from meshcnn.models.layers.mesh_unpool import *
+import torch
+import torch.nn as nn
+
+# Sparse implementation
+class MeshUnpool(nn.Module):
+    def __init__(self, unroll_target):
+        super(MeshUnpool, self).__init__()
+        self.unroll_target = unroll_target
+
+    def __call__(self, features, meshes):
+        return self.forward(features, meshes)
+
+    def pad_groups(self, group, unroll_start):
+        start, end = group.shape
+        padding_rows =  unroll_start - start
+        padding_cols = self.unroll_target - end
+        if padding_rows != 0 or padding_cols !=0:
+            padding = nn.ConstantPad2d((0, padding_cols, 0, padding_rows), 0)
+            group = padding(group)
+        return group
+
+    def pad_sparse_groups(self, group):
+        start, end = group.shape
+        padding_rows =  self.unroll_target - start
+        padding_cols = self.unroll_target - end
+        if padding_rows != 0 or padding_cols !=0:
+            group.sparse_resize_((self.unroll_target, self.unroll_target), 2, 0)
+        return group
+
+    def pad_occurrences(self, occurrences):
+        padding = self.unroll_target - occurrences.shape[0]
+        if padding != 0:
+            padding = nn.ConstantPad1d((0, padding), 1)
+            occurrences = padding(occurrences)
+        return occurrences
+
+    def pad_mask(self,mask):
+        padding = self.unroll_target - mask.shape[0]
+        if padding != 0:
+            padding = nn.ConstantPad1d((0, padding), False)
+            mask = padding(mask)
+        return mask
+
+    def forward(self, features, meshes):
+        batch_size, nf, edges = features.shape
+        groups = [self.pad_groups(mesh.get_groups(), edges) for mesh in meshes]
+        unroll_mat = torch.cat(groups, dim=0).view(batch_size, edges, -1)
+
+        sparse_groups = [self.pad_sparse_groups(mesh.get_sparse_groups()) for mesh in meshes]
+        sparse_unroll_mat = torch.stack(sparse_groups)
+
+        occurrences = [self.pad_occurrences(mesh.get_occurrences()) for mesh in meshes]
+        occurrences = torch.cat(occurrences, dim=0).view(batch_size, 1, -1)
+        dense_occurrences = occurrences.expand(unroll_mat.shape)
+
+
+        unroll_mat = unroll_mat / dense_occurrences
+        unroll_mat = unroll_mat.to(features.device)
+
+        #sparse_unroll_mat = sparse_unroll_mat / sparse_ocurrences
+        sparse_unroll_mat = sparse_unroll_mat.to(features.device)
+
+        for mesh in meshes:
+            mesh.unroll_gemm()
+
+        res = torch.matmul(features, unroll_mat)
+
+        masks = torch.cat([self.pad_mask(mesh.get_group_mask()) for mesh in meshes],dim=0).view(batch_size,1,self.unroll_target).expand(-1,nf,-1).transpose(2,1)
+        padded_features = torch.zeros((batch_size,self.unroll_target,nf),device=features.device)
+        test = padded_features[masks]
+        padded_features[masks] = features.transpose(2,1).reshape(-1)
+
+        sparseRes = torch.sparse.mm(sparse_unroll_mat.transpose(1,0), padded_features).transpose(1, 0)
+        sparseRes = sparseRes / occurrences.expand(sparseRes.shape)
+
+        return res
+
 
