38c15739ead55d5f4c5ac5e5961eeb66d8077552
Andres Mandado <andres.mandado-almajano@here.com>
2020-12-28 12:54:35 +0100

    Mesh visualization script improvements (WIP)
    - Save result html pages in specified directory or tmp.
    - Open html pages automatically on default browser.

---
+++ development/scripts/abc/seg/view.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+python util/mesh_viewer.py --files "$@"
+
+
--- development/util/mesh_viewer.py
+++ development/util/mesh_viewer.py
@@ -1,83 +1,15 @@
-import mpl_toolkits.mplot3d as a3
 import matplotlib.colors as colors
-import pylab as pl
 import numpy as np
 import meshplot as mp
+import webbrowser
+import tempfile
 import os
+import sys
 
 V = np.array
-r2h = lambda x: colors.rgb2hex(tuple(map(lambda y: y / 255., x)))
-surface_color = r2h((255, 230, 205))
-edge_color = r2h((90, 90, 90))
-edge_colors = (r2h((15, 167, 175)), r2h((230, 81, 81)), r2h((142, 105, 252)), r2h((248, 235, 57)),
-               r2h((51, 159, 255)), r2h((225, 117, 231)), r2h((97, 243, 185)), r2h((161, 183, 196)))
-
-
-
-
-def init_plot():
-    ax = pl.figure().add_subplot(111, projection='3d')
-    # hide axis, thank to
-    # https://stackoverflow.com/questions/29041326/3d-plot-with-matplotlib-hide-axes-but-keep-axis-labels/
-    ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-    ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-    ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-    # Get rid of the spines
-    ax.w_xaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-    ax.w_yaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-    ax.w_zaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-    # Get rid of the ticks
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_zticks([])
-    return (ax, [np.inf, -np.inf, np.inf, -np.inf, np.inf, -np.inf])
-
-
-def update_lim(mesh, plot):
-    vs = mesh[0]
-    for i in range(3):
-        plot[1][2 * i] = min(plot[1][2 * i], vs[:, i].min())
-        plot[1][2 * i + 1] = max(plot[1][2 * i], vs[:, i].max())
-    return plot
-
-
-def update_plot(mesh, plot):
-    if plot is None:
-        plot = init_plot()
-    return update_lim(mesh, plot)
-
-
-def surfaces(mesh, plot):
-    vs, faces, edges = mesh
-    vtx = vs[faces]
-    edgecolor = edge_color if not len(edges) else 'none'
-    tri = a3.art3d.Poly3DCollection(vtx, facecolors=surface_color +'55', edgecolors=edgecolor,
-                                    linewidths=.5, linestyles='dashdot')
-    plot[0].add_collection3d(tri)
-    return plot
-
-
-def segments(mesh, plot):
-    vs, _, edges = mesh
-    for edge_c, edge_group in enumerate(edges):
-        for edge_idx in edge_group:
-            edge = vs[edge_idx]
-            line = a3.art3d.Line3DCollection([edge],  linewidths=.5, linestyles='dashdot')
-            line.set_color(edge_colors[edge_c % len(edge_colors)])
-            plot[0].add_collection3d(line)
-    return plot
-
-
-def plot_mesh(mesh, *whats, show=True, plot=None):
-    for what in [update_plot] + list(whats):
-        plot = what(mesh, plot)
-    if show:
-        li = max(plot[1][1], plot[1][3], plot[1][5])
-        plot[0].auto_scale_xyz([0, li], [0, li], [0, li])
-        pl.tight_layout()
-        pl.show()
-    return plot
-
+surface_color = np.array(colors.to_rgb(colors.CSS4_COLORS['lightgrey']))
+edge_colors = ("red","green", "blue","cyan","magenta","yellow","orange","purple","black")
+# ['Plane','Revolution', 'Cylinder','Extrusion','Cone','Other','Sphere','Torus','BSpline']
 
 def parse_obje(obj_file, scale_by):
     vs = []
@@ -129,24 +61,28 @@ def parse_obje(obj_file, scale_by):
     return (vs, faces, edges), scale_by
 
 
-def view_meshes(*files, offset=.2):
-    plot = None
-    max_x = 0
-    scale = 0
-    mp.offline()
-    shading = {"width": 1200,
-               "height": 800}
+def view_meshes(files, outPath):
+
+    tmpDir = None
+    if (len(outPath) == 0):
+        outPath = tempfile.mkdtemp()
+
+    mp.website()
+    shading = {"width": 1600,
+               "height": 900,
+               "colormap": "caca"}
 
 
     p = None
     meshCount = len(files)
     meshIndex = 0
+
     for file in files:
-        mesh, scale = parse_obje(file, scale)
+        mesh, scale = parse_obje(file, 0)
         #if (p == None):
             #p = mp.subplot(mesh[0], mesh[1], shading=shading,s=[1,meshCount,meshIndex])
             #mp.plot(mesh[0], mesh[1], shading=shading)
-        p = mp.plot(mesh[0], mesh[1], shading=shading,return_plot=True)
+        p = mp.plot(mesh[0], mesh[1],surface_color, shading=shading,return_plot=True)
         colorIndex = 0
         for edgeClass in mesh[2]:
             if (len(edgeClass) > 0):
@@ -155,21 +91,22 @@ def view_meshes(*files, offset=.2):
         # else:
         #     mp.subplot(mesh[0],mesh[1], shading=shading,data=p,s=[1,meshCount,meshIndex])
         meshIndex += 1
-        #outFile = os.path.splitext(file)[0] + ".html"
+        outFile = os.path.join(outPath,os.path.basename(os.path.splitext(file)[0]) + ".html")
         # max_x_current = mesh[0][:, 0].max()
         # mesh[0][:, 0] += max_x + offset
         # plot = plot_mesh(mesh, surfaces, segments, plot=plot, show=file == files[-1])
         # max_x += max_x_current + offset
-    p.save()
+        p.save(outFile)
+        webbrowser.open(outFile)
 
 if __name__=='__main__':
     import argparse
+
     parser = argparse.ArgumentParser("view meshes")
-    parser.add_argument('--files', nargs='+', default=['checkpoints/human_seg/meshes/shrec__14_0.obj',
-                                                       'checkpoints/human_seg/meshes/shrec__14_3.obj'], type=str,
-                        help="list of 1 or more .obj files")
+    parser.add_argument('--files', nargs='+', type=str, help="list of 1 or more .obj files")
+    parser.add_argument('--outPath', type=str,  default="", help="output path for generated .html file")
     args = parser.parse_args()
 
     # view meshes
-    view_meshes(*args.files)
+    view_meshes(args.files, args.outPath)
 
