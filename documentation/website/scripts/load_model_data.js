3a4e53d0775085f64a7e6d9ce80321efa37872ba
Andres Mandado <andres.mandado-almajano@here.com>
2021-01-17 19:34:09 +0100

    Update website for intermediate presentation

---
+++ documentation/website/scripts/load_model_data.js
@@ -0,0 +1,14 @@
+scripts = document.getElementsByClassName('model');
+
+for(var i=0; i < scripts.length; ++i) {
+    const script = scripts[i];
+    var src = script.getAttribute("src")
+     fetch(src).then(response => {
+       if (!response.ok) {
+           throw new Error("HTTP error " + response.status);
+       }
+       return response.text();
+    }).then(text => {
+        script.innerHTML = text;
+    });
+}
