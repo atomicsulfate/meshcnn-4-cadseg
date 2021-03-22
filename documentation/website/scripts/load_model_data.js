419f908aba861d8c8165d528538bb0d1a5695443
Andres Mandado <andres.mandado-almajano@here.com>
2021-03-22 20:27:30 +0100

    Update web documentation

--- documentation/website/scripts/load_model_data.js
+++ documentation/website/scripts/load_model_data.js
@@ -1,15 +1,27 @@
 scripts = document.getElementsByClassName('model');
 
+promises = [];
 for(var i=0; i < scripts.length; ++i) {
     const script = scripts[i];
     var src = script.getAttribute("src")
-     fetch(src).then(response => {
+    promises.push(fetch(src).then(response => {
        if (!response.ok) {
            throw new Error("HTTP error " + response.status);
        }
        return  response.text();
-    }).then(text => {
-           console.log(`Loaded script with size ${text.length}`);
-        script.innerHTML = text;
-    });
+    }));
 }
+Promise.all(promises).then((scriptCodes) => {
+    for(var i=0; i < scripts.length; ++i) {
+    var script = scripts[i];
+    var scriptCode = scriptCodes[i];
+    //console.log(`Loaded script with size ${scriptCode.length}`);
+    script.innerHTML = scriptCode;
+    }
+
+    const embedScript = document.createElement('script');
+    document.body.appendChild(embedScript);
+    //embedScript.async = true;
+    embedScript.src = 'https://unpkg.com/@jupyter-widgets/html-manager@^0.18.0/dist/embed-amd.js';
+});
+
